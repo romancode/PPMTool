af83dad668fde14682973210a08bf1acd1289a29
romancode <ar_roman@yahoo.com>
2019-02-14 00:04:20 -0500

    Custom Exception for uniuqu identifier

--- src/main/java/com/krosstek/ppmtool/services/ProjectService.java
+++ src/main/java/com/krosstek/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.krosstek.ppmtool.domain.Project;
+import com.krosstek.ppmtool.exceptions.ProjectIdException;
 import com.krosstek.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -13,6 +14,14 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exist!");
+		}
+		
+		
 	}
 }
